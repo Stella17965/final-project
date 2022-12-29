7264478b7a2774ada6381f091dd3ef4cbde3ef53
Stella17965 <111170156+Stella17965@users.noreply.github.com>
2022-12-29 20:47:04 +0800

    first commit

---
+++ final project/app/src/main/java/com/stella/finalproject/MainActivity2.java
@@ -0,0 +1,30 @@
+package com.stella.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private Button btn_drawwave;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        btn_drawwave=findViewById(R.id.btn_drawwave);
+        Intent intent_toPage3 = new Intent(this,MainActivity3.class);
+
+        btn_drawwave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent_toPage3);
+            }
+        });
+    }
+}
