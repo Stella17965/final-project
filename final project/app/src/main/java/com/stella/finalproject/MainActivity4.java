3ac481a201989e3ffa3fca52cb1ab0cd1deebe9c
Stella17965 <111170156+Stella17965@users.noreply.github.com>
2022-12-31 00:22:28 +0800

    Revert "add page6"
    This reverts commit 4b7a9acfb13fa671242b49c8384ffd9f49761fd1.

--- final project/app/src/main/java/com/stella/finalproject/MainActivity4.java
+++ final project/app/src/main/java/com/stella/finalproject/MainActivity4.java
@@ -9,28 +9,19 @@ import android.widget.Button;
 
 public class MainActivity4 extends AppCompatActivity {
 
-    private Button btn_1,btn_2;
+    private Button btn_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
         btn_1=findViewById(R.id.btn_1);
-        btn_2=findViewById(R.id.btn_2);
         Intent intent_toPage5 = new Intent(this,MainActivity5.class);
-        Intent intent_toPage6 = new Intent(this,MainActivity6.class);
 
         btn_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(intent_toPage5);
-            }
-        });
-
-        btn_2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(intent_toPage6);
             }
         });
 
